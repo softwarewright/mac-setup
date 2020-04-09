a786533621a24a08672f93cc26584db23a98b68e
Darrius Wright <49100831+softwarewright@users.noreply.github.com>
2020-04-08 18:03:28 -0600

    Fixing mac install issues

--- setup.sh
+++ setup.sh
@@ -4,6 +4,16 @@ xcode-select --install
 brew update
 
 brew install watchman zsh git nvm awscli terraform jq kubectl
+
+sudo chmod -R 755 zsh
+for f in $(compaudit);do sudo chown $(whoami):admin $f;done;
+
+mkdir ~/.nvm
+
+echo 'export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion' >> ~/.zshrc
+
 nvm install node && nvm use default
 
 npm install -g typescript ts-node
@@ -13,7 +23,7 @@ sudo sh -c "echo $(which zsh) >> /etc/shells" && chsh -s $(which zsh)
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts && ./install.sh && cd .. && rm -rf fonts
 
-brew tap caskroom/cask
+brew tap homebrew/cask-cask
 brew cask install docker \
 spectacle \
 google-chrome \
