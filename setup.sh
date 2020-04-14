7424d86c2ae7aa2754ba1ad7f0faed187f7df3f5
Darrius Wright <49100831+softwarewright@users.noreply.github.com>
2020-04-14 09:46:28 -0600

    Setting zsh theme

--- setup.sh
+++ setup.sh
@@ -10,7 +10,8 @@ for f in $(compaudit);do sudo chown $(whoami):admin $f;done;
 
 mkdir ~/.nvm
 
-echo 'export NVM_DIR="$HOME/.nvm"
+echo 'ZSH_THEME=agnoster
+  export NVM_DIR="$HOME/.nvm"
   [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion' >> ~/.zshrc
 
